e9648bb103426968ed9c5f5ba0b8eebd7a0e1338
Denis <mentalitapp@gmail.com>
2025-12-13 19:45:11 -0500

    Linked the creation process of pairs, aisles and sections

--- app/models/aisle.rb
+++ app/models/aisle.rb
@@ -1,4 +1,26 @@
 class Aisle < ApplicationRecord
   belongs_to :pair
   has_many :sections
+
+
+  after_create :create_sections
+
+  private
+
+  def create_sections
+    section_range = (1..aisle_sections.to_i).to_a
+
+    section_range.each do |sec |
+      sections.create!(
+        section_num: sec,
+        section_depth: aisle_depth,
+        section_height: aisle_height ,
+        section_width: aisle_section_width 
+        )
+      end
+  end
+
+
 end
+
+
--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,4 +1,28 @@
 class Pair < ApplicationRecord
   belongs_to :store
   has_many :aisles
+
+  after_create :create_aisles
+
+  private
+
+  def create_aisles
+    aisle_numbers.each do |num|
+      aisles.create!(
+        aisle_num: num,
+        aisle_depth: (pair_depth/2) ,
+        aisle_height: pair_height,
+        aisle_section_width: pair_section_width,
+        aisle_sections: pair_sections 
+        )
+      end
+  end
+
+
+
+
+
+  def aisle_numbers
+    pair_nums.split(",").map(&:strip)
+  end
 end
