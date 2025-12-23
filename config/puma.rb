649989ae9f37e9d7f19de36309de5151916742d8
denis <mentalitapp@gmail.com>
2025-12-22 22:43:27 -0500

    Fix Puma preload single-worker crash on Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -11,22 +11,10 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-rails_env = ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-
-
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if rails_env == "development"
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3000 }
 
-# Specifies the `environment` that Puma will run in.
-environment rails_env
-
-# Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart   
+plugin :tmp_restart 
